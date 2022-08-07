42fc470e3c5bf2a5c00b9d067eed7687ae12f346
jewel <snowdecember079@gmail.com>
2022-08-07 18:33:40 -0400

    commit tree f432849078325339d52f8014d46dab7f1d7bc153

--- js/demo.js
+++ js/demo.js
@@ -1,4 +1,77 @@
-console.log('hi')
 
-const URL_PREFIX='http://localhost:3000/';
-let page=1;const getMonsters=a=>{console.log('get monsters function'),fetch(URL_PREFIX+`monsters/?_limit=50&_page=${a}`).then(b=>b.json()).then(b=>{document.querySelector('#monster-container').innerHTML='';for(let c=0;c<b.length;c++)console.log('monster',b[c]),createMonsterCard(b[c])})},createMonsterCard=a=>{let b=document.createElement('div'),c=document.createElement('h2'),d=document.createElement('h4'),e=document.createElement('p');c.innerHTML=`${a.name}`,d.innerHTML=`Age: ${a.age}`,e.innerHTML=`Bio: ${a.description}`,b.appendChild(c),b.appendChild(d),b.appendChild(e),document.querySelector('#monster-container').appendChild(b)},createMonsterForm=()=>{const a=document.createElement('form'),b=document.createElement('input'),c=document.createElement('input'),d=document.createElement('input'),e=document.createElement('button');a.id='monster-form',b.id='name',c.id='age',d.id='description',b.placeholder='name...',c.placeholder='age...',d.placeholder='description...',e.innerHTML='Create',a.appendChild(b),a.appendChild(c),a.appendChild(d),a.appendChild(e),document.getElementById('create-monster').appendChild(a),addSubmitEventListener()},addSubmitEventListener=()=>{document.querySelector('#monster-form').addEventListener('submit',a=>{a.preventDefault(),console.log('submitted',getFormData()),postNewMonster(getFormData()),clearForm()})},getFormData=()=>{let a=document.querySelector('#name'),b=document.querySelector('#age'),c=document.querySelector('#description');return{name:a.value,age:parseFloat(b.value),description:c.value}},postNewMonster=a=>{let b=URL_PREFIX+`monsters`,c={method:'POST',headers:{'Content-type':'application/json',Accept:'application/json'},body:JSON.stringify(a)};fetch(b,c).then(d=>d.json()).then(d=>console.log('new monster',d))},clearForm=()=>{document.querySelector('#monster-form').reset()},addNavListeners=()=>{let a=document.querySelector('#back'),b=document.querySelector('#forward');a.addEventListener('click',()=>{pageDown()}),b.addEventListener('click',()=>{pageUp()})},pageUp=()=>{page++,getMonsters(page)},pageDown=()=>{1<page?(page--,getMonsters(page)):alert('Aint no monsters here')},init=()=>{getMonsters(),createMonsterForm(),addNavListeners()};document.addEventListener('DOMContentLoaded',init);
+const BASE_URL = "https://raw.githubusercontent.com/Dane-Dawson/json-server-collection/main/dnd-5e-monsters/db.json"
+const dropMenu = document.getElementById("drop-menu")
+const imageContainer = document.getElementById('show-pic')
+const selected = document.getElementById("selected")
+let monsterList = []
+
+const getMonsters = () => {
+    return fetch(BASE_URL)
+    .then(res => res.json())
+}
+
+const appendMonsters = (data=>{
+    monsterList = data.monsters
+        for(let i = 0; i < monsterList.length; i++){
+            let opt = monsterList[i].name
+            let el = document.createElement("option")
+            el.innerText = opt
+            dropMenu.appendChild(el)
+        }
+})
+
+const init = () => {
+    getMonsters()
+    .then(appendMonsters)
+}
+
+document.addEventListener('DOMContentLoaded', init())
+
+
+
+
+
+const createCard = (result) => {
+  let div = document.createElement("div")
+  div.className = "container"
+  let images = document.createElement("img")
+  images.src = result.img_url
+  images.className = "image"
+  let actions = document.createElement("p")
+  actions.innerHTML = result.Actions
+  actions.className = "actions"
+  let diceLiker = document.createElement("img")
+  diceLiker.className = "empty-heart"
+  diceLiker.src= "pics/dice-unliked.png"
+  imageContainer.appendChild(div);
+  div.appendChild(images)
+  div.appendChild(actions)
+  div.appendChild(diceLiker)
+  imageContainer.append(document.createElement("br"))
+}
+
+dropMenu.addEventListener("change",(e) => {
+  result = monsterList.find(({name}) => name === `${e.target.value}`)
+  createCard(result)
+})
+
+const beholderImg = document.querySelector(".beholder")
+
+beholderImg.addEventListener("dblclick",() => {
+  bugbearObj = monsterList[58]
+  createCard(bugbearObj)
+})
+
+
+
+imageContainer.addEventListener('click', function (e) {
+  if (e.target.classList.contains('empty-heart')) {
+    e.target.src = "pics/dice-liked.png"
+    e.target.className = 'full-heart'
+  } else {
+    if (e.target.classList.contains('full-heart')) {
+        e.target.src = "pics/dice-unliked.png"
+        e.target.className = 'empty-heart'
+    }
+  }
+});
